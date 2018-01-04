2752786bb64019c41138bad823c3a07880709ed9
me <aldompe95@gmail.com>
2018-01-04 16:12:26 -0600

    Add bootstrap flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def bootstrap_class_for flash_type
+    {
+      success: 'alert-success', 
+      notice: 'alert-info', 
+      danger: 'alert-danger'
+    } [flash_type.to_sym] || flash_type.to_s
+  end
 end
