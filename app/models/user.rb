85a78f9eed83b03afbfb0d8066d42d6900d0d8ef
me <aldompe95@gmail.com>
2018-01-05 12:24:16 -0600

    Set default role on create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :beer_logs
   has_many :beers, through: :beer_logs
+
+  before_create :set_default_role
+  
+  private
+  
+  def set_default_role
+    self.add_role 'beer_lover'
+  end
 end
